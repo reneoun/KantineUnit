48a2b9a3399ffb168bda3310ba23c6bd9430603d
Robbert Tamminga <robbert_tamminga@hotmail.com>
2019-05-22 14:01:09 +0200

    RT: week 1 opgaven af. alles correct.

--- src/KassaRij.java
+++ src/KassaRij.java
@@ -3,56 +3,31 @@ import java.util.LinkedList;
 
 public class KassaRij {
 
-    /**
-     * Constructor
-     */
     ArrayList<Dienblad> positieInRij;
 
 
     public KassaRij() {
-        // method body omitted
-        //ArrayList<String> dienblad = new ArrayList<String>()
         positieInRij = new ArrayList<>();
     }
 
-    /**
-     * Persoon sluit achter in de rij aan
-     *
-     * @param klant
-     */
     public void sluitAchteraan(Dienblad klant) {
-        // method body omitted
-        //Dienblad.add();
         positieInRij.add(klant);
-
     }
 
-    /**
-     * Indien er een rij bestaat, de eerste klant uit
-     * de rij verwijderen en retourneren.
-     * Als er niemand in de rij staat geeft deze null terug.
-     *
-     * @return Eerste klant in de rij of null
-     */
     public Dienblad eerstePersoonInRij() {
-        // method body omitted
         if(positieInRij.get(0) == null) {
             return null;
         }
+        Dienblad eerste = positieInRij.get(0);
         positieInRij.remove(0);
+        return eerste;
     }
 
-    /**
-     * Methode kijkt of er personen in de rij staan.
-     *
-     * @return Of er wel of geen rij bestaat
-     */
     public boolean erIsEenRij() {
         if(positieInRij.size() > 0){
             return true;
         }    else{
             return false;
         }
-
     }
 }
